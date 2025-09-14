4fc8ddb8422e14e66d871a228dcab3029a37c859
Clara <mariaclaraserafimsilva09@gmail.com>
2025-09-14 13:12:02 -0300

    Create lista3.java

---
+++ lista3.java
@@ -0,0 +1,24 @@
+package teste12025;
+
+public class teste1 {
+    public static void main(String[] args) {
+     //questão 1
+    	  byte b = 10;
+          short s = 200;
+          int i = 1000;
+          long l = 100000L;
+          float f = 5.5f;
+          double d = 10.55;
+          char c = 'A';
+          boolean bool = true;
+
+          System.out.println(b);
+          System.out.println(s);
+          System.out.println(i);
+          System.out.println(l);
+          System.out.println(f);
+          System.out.println(d);
+          System.out.println(c);
+          System.out.println(bool);
+    }
+}
