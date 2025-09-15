8a81739de2482caedd72c3fc84f8f8b09f552dc3
Clara <mariaclaraserafimsilva09@gmail.com>
2025-09-15 07:47:00 -0300

    Create lista3,5.java

---
+++ lista3,5.java
@@ -0,0 +1,17 @@
+package teste12025;
+
+import java.util.Scanner;
+
+public class teste1 {
+    public static void main(String[] args) {
+    	//questao 5
+    	
+    	
+    	Scanner scanner = new Scanner(System.in);
+        double n9 = scanner.nextDouble();
+        int n10 = (int)n9;
+        System.out.println(n10);
+    	  
+    	    }
+    	}
+
