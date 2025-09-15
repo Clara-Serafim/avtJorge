58c873c3fb1bd4e69b7235e11746f02e3ec3d5ad
Clara <mariaclaraserafimsilva09@gmail.com>
2025-09-15 07:50:47 -0300

    Create lista3,6.java

---
+++ lista3,6.java
@@ -0,0 +1,18 @@
+package teste12025;
+
+import java.util.Scanner;
+
+public class teste1 {
+    public static void main(String[] args) {
+    	//questao 6
+    		
+    	Scanner scanner = new Scanner(System.in);
+    	System.out.print("Digite um número: ");
+        int n = scanner.nextInt();
+        double d = n;
+        System.out.println("Divisão inteira: " + (5 / 2));
+        System.out.println("Divisão double: " + (5 / 2.0));
+    	  
+    	    }
+    	}
+
