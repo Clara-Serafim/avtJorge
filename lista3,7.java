92b193c86caa7afeb302d6608a84f85d15684bc4
Clara <mariaclaraserafimsilva09@gmail.com>
2025-09-15 07:55:17 -0300

    Create lista3,7.java

---
+++ lista3,7.java
@@ -0,0 +1,16 @@
+package teste12025;
+
+import java.util.Scanner;
+
+public class teste1 {
+    public static void main(String[] args) {
+    	//questao 7
+    		
+        int numeroInt = 327;
+        byte numeroByte = (byte) numeroInt;
+        System.out.println(numeroInt);
+        System.out.println(numeroByte);
+    	  
+    	    }
+    	}
+
